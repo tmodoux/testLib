87a91a1e3e68aae5de6b93f7bc70a18cb3e38344
Thieb Modoux <Thieb Modoux>
2016-07-28 17:48:29 +0200

    Initial commit

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.pryv.Pryv;
+
+public class MyClass {
+    public static void main(String [] args)
+    {
+        System.out.println("hello "+ Pryv.DOMAIN);
+    }
+}
